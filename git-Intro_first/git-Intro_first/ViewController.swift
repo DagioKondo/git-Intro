451d359c2d4041391cf1607f72d936e9d549a055
masappu <njzzmmi6tn@icloud.com>
2021-12-05 16:31:48 +0900

    add, commitのコマンドを追加
    口座3−4を受講したから

--- git-Intro_first/git-Intro_first/ViewController.swift
+++ git-Intro_first/git-Intro_first/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //＜変更点の追加＞,git add,git commit
 
 
 }
